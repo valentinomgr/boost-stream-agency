116152515aee5732916126805cafa329231d370b
valentinomgr <168256396+valentinomgr@users.noreply.github.com>
2026-03-29 02:14:08 +0200

    Delete scripts directory

--- scripts/update-footer-year.js
+++
@@ -1,8 +0,0 @@
-export function updateFooterYear() {
-  const yearElement = document.getElementById("year");
-
-  // Перевіряємо, чи існує елемент, і лише тоді виконуємо присвоєння.
-  if (yearElement) {
-    yearElement.textContent = new Date().getFullYear();
-  }
-}
